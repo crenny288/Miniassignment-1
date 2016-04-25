011f6f83e29b007c7034ea34147461f46a9259ce
Ben Sterrett <raynjamin@gmail.com>
2016-04-25 05:43:01 -0400

    Removing class files

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -1,23 +1,9 @@
 package com.theironyard.clt;
 
 public class Main extends Object {
-    /**
-     * This main method should perform a variety of actions:
-     * <ol>
-     *     <li>Assign a variable of type `Animal` a new `Bird`</li>
-     *     <li>Set a new number of legs for the bird.</li>
-     *     <li>Call `flapWings()` on the bird object.</li>
-     * </ol>
-     *
-     * @param args: The parameters from the command line
-     */
+
     public static void main(String[] args) {
-        Animal bird = new Bird(2, 2);
 
-        bird.setNumLegs(3);
-
-        bird.setNumArms(2);
-        ((Bird)bird).flapWings();
     }
 }
 
